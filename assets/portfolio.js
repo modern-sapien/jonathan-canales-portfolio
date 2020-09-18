2c21de24d8f8a7a1c9160c2a79b8569184be9423
Jonathan Canales <joncanales2@gmail.com>
2020-09-18 16:39:28 -0400

    made an images folder, targeted global variables in my JS file, planning on dynamically generating all of the pages & clicking through a portfolio after display of a *gallery*.

---
+++ assets/portfolio.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+
+// ===============
+// GLOBAL VARIABES
+// ===============
+
+// Top Nav Variables
+// =================
+var contactNav = $("#contact-html");
+var portfolioNav = $("#portfolio-html");
+var indexNav = $("#index-html");
+var contactNav = $("#contact-html");
+
+var projectBtn = $("#projects-button");
+
+
+console.log("ready to do things");
+
+
+projectBtn.on("click", function()   {
+    console.log("I've been clicked!!!")
+})
+
+
+
+
+
+});
