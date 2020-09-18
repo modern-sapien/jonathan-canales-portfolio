16bbff452bc034f40baf00f20e107df2e9a5774a
Jonathan Canales <joncanales2@gmail.com>
2020-09-18 16:51:58 -0400

    I decided not to dynamically generate all of the content as I only have today to do this project. navigating to web pages seems like a better approach in this instance.

--- assets/portfolio.js
+++ assets/portfolio.js
@@ -4,22 +4,30 @@ $(document).ready(function() {
 // GLOBAL VARIABES
 // ===============
 
-// Top Nav Variables
+// Top Nav Target
 // =================
 var contactNav = $("#contact-html");
 var portfolioNav = $("#portfolio-html");
 var indexNav = $("#index-html");
 var contactNav = $("#contact-html");
 
-var projectBtn = $("#projects-button");
+var leftContainer = $("#left-container");
+var rightContainer = $("#right-container");
 
+// Button Target
+var projectBtn = $("#projects-button");
 
 console.log("ready to do things");
 
-
+// PROJECTS BUTTON CLICK LISTENER
 projectBtn.on("click", function()   {
     console.log("I've been clicked!!!")
-})
+    leftContainer.empty();
+    rightContainer.empty();
+});
+
+
+
 
 
 
